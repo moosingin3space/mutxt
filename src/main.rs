03232805a46c9c3ac6389ef8c405a302c908bc07
Nathan Moos <moosingin3space@gmail.com>
2016-07-24 13:05:15 -0700

    made window resizing work

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,29 @@ mod editor;
 use std::env;
 use std::io;
 use std::io::{Write};
+use std::sync::atomic::{AtomicBool, ATOMIC_BOOL_INIT, Ordering};
 
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use termion::terminal_size;
 
+use nix::sys::signal;
+
 const HELP_MSG: &'static str = "HELP: CTRL-O to open a file, CTRL-S to save the current file, and CTRL-Q to quit.";
+#[allow(non_upper_case_globals)]
+static ShouldResizeWindow: AtomicBool = ATOMIC_BOOL_INIT;
 
 macro_rules! render {
     ($editor:ident, $stdout:ident) => {
         $editor.render(&mut $stdout).expect("Failed to render");
         $stdout.flush().unwrap();
+    }
+}
+
+extern fn signal_handler(signo: i32) {
+    if signo == signal::SIGWINCH {
+        ShouldResizeWindow.store(true, Ordering::Relaxed);
     }
 }
 
@@ -33,6 +44,14 @@ fn main() {
     editor.status_message = Some(HELP_MSG.to_owned());
     editor.open_file(&filename).expect("Could not open the file provided");
     render!(editor, stdout);
+
+    unsafe {
+        signal::sigaction(signal::SIGWINCH,
+                          &signal::SigAction::new(
+                              signal::SigHandler::Handler(signal_handler),
+                              signal::SaFlags::empty(),
+                              signal::SigSet::empty())).expect("failed to set signal handler");
+    }
 
     for c in stdin.keys() {
         match c.unwrap() {
@@ -84,6 +103,11 @@ fn main() {
             _ => {
                 // TODO insert the char
             }
+        }
+        if ShouldResizeWindow.compare_and_swap(true, false, Ordering::Relaxed) {
+            let (screen_cols, screen_rows) = terminal_size().expect("Could not get the terminal size");
+            editor.set_screen_size(screen_rows as usize, screen_cols as usize);
+            render!(editor, stdout);
         }
     }
 
